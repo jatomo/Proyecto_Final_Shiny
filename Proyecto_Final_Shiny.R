dd193cf775f3311ef9681e6027c7ba54dd6dbecd
Jahaira Mora Corrales <jahaira.mora.corrales@gmail.com>
2023-12-19 21:25:07 -0600

    cambios en código

--- Proyecto_Final_Shiny.R
+++ Proyecto_Final_Shiny.R
@@ -3,36 +3,32 @@ library(shinydashboard)
 library(dplyr)
 library(ggplot2)
 library(plotly)
-library(leaflet)
-library(shinyjs)
 library(readr)
 library(tidyr)
 
-
-
 imdb <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2020/2020-02-19/ranking_imdb.csv")
-head(imdb)
-View(imdb)
-
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Explorador de Películas IMDB"),
+  dashboardHeader(title = "Explorador de Películas IMDB",titleWidth = 300),
   
-  dashboardSidebar(
-    selectInput("genre", "Género:", choices = unique(imdb$genero)),
-    sliderInput("rating", "Puntaje:", min = 1, max = 10, value = c(1, 10)),
-    sliderInput("votes", "Cantidad de Votos:", min = 0, max = max(imdb$votos), value = c(0, max(imdb$votos))),
-    sliderInput("year", "Año de Estreno:", min = min(imdb$anio), max = max(imdb$anio), value = c(min(imdb$anio), max(imdb$anio))),
-    shinyalert("download_alert", "Descarga Exitosa", "Los datos han sido descargados exitosamente.", type = "success")
+  dashboardSidebar(width = 300,
+                   selectInput("genre", "Género:", choices = unique(imdb$genero)),
+                   sliderInput("rating", "Puntaje:", min = 1, max = 10, value = c(1, 10)),
+                   sliderInput("votes", "Cantidad de Votos:", min = 0, max = max(imdb$votos), value = c(0, max(imdb$votos))),
+                   sliderInput("year", "Año de Estreno:", min = min(imdb$anio), max = max(imdb$anio), value = c(min(imdb$anio), max(imdb$anio))),
+                   
+                   fluidRow(
+                     column(width = 12, offset = 2,
+                            downloadButton("descargarDatos", "Descargar Datos"),
+                            useShinyjs()
+                     )
+                   )
   ),
-  
   dashboardBody(
-    fluidRow(
-      box(plotlyOutput("scatter_plot")),
-      box(downloadButton("download_data", "Descargar Datos"))
-    )
+    column(12, plotlyOutput("scatter_plot"))
   )
 )
+
 
 server <- function(input, output, session) {
   filtered_data <- reactive({
@@ -45,30 +41,25 @@ server <- function(input, output, session) {
   
   output$scatter_plot <- renderPlotly({
     plot_ly(data = filtered_data(), x = ~ranking, y = ~puntaje, color = ~anio, type = "scatter", mode = "markers", text = ~titulo) %>%
-      layout(title = "Ranking vs. Puntaje de Películas IMDB",
+      layout(title = "Ranking vs.Puntaje de Películas IMDB",
              xaxis = list(title = "Posición en el Ranking"),
              yaxis = list(title = "Puntaje"))
   })
   
+  observe({
+    if (nrow(filtered_data()) == 0) {
+      shinyjs::disable("descargarDatos")
+    } else {
+      shinyjs::enable("descargarDatos")
+    }
+  })
   
-  output$download_data <- downloadHandler(
+  output$descargarDatos <- downloadHandler(
     filename = function() {
-      paste("datos_filtrados_", Sys.Date(), ".csv", sep = "")
+      paste("películas_seleccionadas/", "_", input$genre, ".csv", sep = "")
     },
     content = function(file) {
       write_csv(filtered_data(), file)
-    }
-  )
-  
-  observeEvent(input$download_data, 
-  )
+    } )
 }
-
-shinyApp(ui, server)
-
-
-
-
-
-
-
+shinyApp(ui = ui, server = server)
